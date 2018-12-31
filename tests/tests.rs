1bfd820995bb7b2ebae9c9b706eab00d537c0bb0
Gaëtan Lehmann <gaetan.lehmann@gmail.com>
2018-12-31 16:46:21 +0100

    actually test the deduplication

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,7 +1,11 @@
 extern crate assert_cmd;
+extern crate assert_fs;
+extern crate lipsum;
 extern crate predicates;
 
 use assert_cmd::prelude::*;
+use assert_fs::prelude::*;
+use lipsum::lipsum;
 use predicates::prelude::*;
 use std::process::Command;
 
@@ -48,6 +52,55 @@ fn test_version() {
         .arg("--version")
         .assert()
         .success()
-        .stdout(predicate::str::is_match(r"hld \d+\.\d+\.\d+").unwrap())
+        .stdout(predicate::str::is_match(r"^hld \d+\.\d+\.\d+\n$").unwrap())
         .stderr(predicate::str::is_empty());
+}
+
+#[test]
+fn test_bad_option() {
+    Command::main_binary()
+        .unwrap()
+        .arg("--foo")
+        .assert()
+        .failure()
+        .stdout(predicate::str::is_empty())
+        .stderr(
+            predicate::str::is_match(r"^error: .+ '--foo'")
+                .unwrap()
+                .and(predicate::str::contains("USAGE:")),
+        );
+}
+
+#[test]
+fn test_deduplication() {
+    let lorem_ipsum = lipsum(100);
+    // set up the test dir
+    let tmp = assert_fs::TempDir::new().unwrap();
+    let foo = tmp.child("foo.txt");
+    let bar = tmp.child("bar.txt");
+    foo.write_str(&lorem_ipsum).unwrap();
+    bar.write_str(&lorem_ipsum).unwrap();
+
+    assert_ne!(inos(foo.path()), inos(bar.path()));
+
+    Command::main_binary()
+        .unwrap()
+        .arg(tmp.child("*.txt").path())
+        .assert()
+        .success()
+        .stdout(predicate::str::is_empty())
+        .stderr(predicate::str::contains("foo.txt").and(predicate::str::contains("bar.txt")));
+
+    assert_eq!(inos(foo.path()), inos(bar.path()));
+}
+
+use std::fs;
+use std::os::linux::fs::MetadataExt as LinuxMetadataExt;
+use std::os::unix::fs::MetadataExt;
+use std::path::Path;
+
+/// returns the inodes of the partition and of the file
+fn inos(path: &Path) -> (u64, u64) {
+    let metadata = fs::metadata(path).unwrap();
+    (metadata.st_dev(), metadata.ino())
 }
