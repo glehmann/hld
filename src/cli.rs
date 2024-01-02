400931cbb78712b1ca0d4e3df7a84d6fd0d073ec
Gaëtan Lehmann <gaetan.lehmann@gmail.com>
2024-01-02 17:29:39 +0100

    simplify clap usage

--- src/cli.rs
+++ src/cli.rs
@@ -13,44 +13,44 @@ pub struct Config {
     pub files: Vec<String>,
 
     /// Files to cache
-    #[arg(short = 'c', long = "cache", number_of_values = 1)]
+    #[arg(short, long = "cache", number_of_values = 1)]
     pub caches: Vec<String>,
 
     /// Cache file
-    #[arg(short = 'C', long = "cache-path")]
-    pub cache_path_opt: Option<PathBuf>,
+    #[arg(short = 'C', long)]
+    pub cache_path: Option<PathBuf>,
 
     /// Clear the cache file
-    #[arg(long = "clear-cache")]
+    #[arg(long)]
     pub clear_cache: bool,
 
     /// Recursively find the files in the provided paths
-    #[arg(short = 'r', long = "recursive")]
+    #[arg(short, long)]
     pub recursive: bool,
 
     /// Don't modify anything on the disk
-    #[arg(short = 'n', long = "dry-run")]
+    #[arg(short = 'n', long)]
     pub dry_run: bool,
 
     /// The linking strategy to use - either hardlink, symlink or reflink
-    #[arg(short = 's', long = "strategy", default_value = "hardlink")]
+    #[arg(short, long, default_value = "hardlink")]
     pub strategy: Strategy,
 
     /// Parallelism level
-    #[arg(short = 'j', long = "parallel")]
+    #[arg(short = 'j', long)]
     pub parallel: Option<usize>,
 
     /// Log level
-    #[arg(short = 'l', long = "log-level", default_value = "info")]
+    #[arg(short = 'l', long, default_value = "info")]
     pub log_level: log::Level,
     /// Generate the completion code for this shell
-    #[arg(long = "completion")]
+    #[arg(long)]
     pub completion: Option<Shell>,
 }
 
 impl Config {
     pub fn cache_path(self: &Self) -> PathBuf {
-        let path = if let Some(ref path) = self.cache_path_opt {
+        let path = if let Some(ref path) = self.cache_path {
             path.clone()
         } else {
             let mut path = ProjectDirs::from("com", "glehmann", "hld")
